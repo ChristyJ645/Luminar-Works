eeb234f4d7d0d3b14d0fcd3ac123d7bb119d7b47
ChristyJ645 <christyjoseph645@gmail.com>
2021-07-03 12:01:40 +0530

    3/7

---
+++ DOCUMENT OBJECT MODEL/calcscript.js
@@ -0,0 +1,11 @@
+//displayNumber()
+let displayNumber=(num)=>inpt.value+=num
+ 
+//evaluateExpression()
+let evaluateExpression=()=>inpt.value=eval(inpt.value);
+
+//clearBox()
+let clearBox=()=>inpt.value="";
+
+//removeNumber()
+let removeNumber=()=>inpt.value=inpt.value.slice(0,-1)
